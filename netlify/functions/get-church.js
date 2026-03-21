4f0d474387cef786d873578ae7ee9a728acd9ba6
AngelaGeary <91739871+AngelaGeary@users.noreply.github.com>
2026-03-21 13:26:08 +0000

    Update get-church.js

--- netlify/functions/get-church.js
+++ netlify/functions/get-church.js
@@ -79,8 +79,10 @@ exports.handler = async (event, context) => {
         meetings: fields['Meetings'] || '',
         notes: fields['Notes'] || '',
         churchImage: fields['Church image'] ? fields['Church image'][0]?.url : null,
-        billingName: fields['Billing name (from Holiday church purchases 2026)'] ? fields['Billing name (from Holiday church purchases 2026)'][0] : null,
-        listingType: fields['Listing type (from Holiday church purchases 2026)'] ? fields['Listing type (from Holiday church purchases 2026)'][0] : null,
+        billingName: fields['Billing name (from Holiday church purchases 2026)'] ? fields['Billing name (from Holiday church purchases 2026)'][0] : 
+                     (fields['Holiday Church online subscribers only'] ? fields['Holiday Church online subscribers only'][0] : null),
+        listingType: fields['Listing type (from Holiday church purchases 2026)'] ? fields['Listing type (from Holiday church purchases 2026)'][0] : 
+                     (fields['Listing type (from Holiday Church online subscribers only)'] ? fields['Listing type (from Holiday Church online subscribers only)'][0] : null),
         included2026: fields['Included 2026'] || ''
       })
     };
